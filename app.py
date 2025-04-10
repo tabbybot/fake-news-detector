eb2d1de975ee6a9b5b07d919b8c5c9ee299ee7a2
tabbybot <reshiram038@gmail.com>
2025-04-11 00:15:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ vectorizer = get_vectorizer()
 st.title("Fake News Detector 📰")
 st.header("Fake News Detector from URL")
 st.write("Note: Our project may not be able to extract news articles from certain websites due to web scraping being blocked")
-st.write("tabbybot et al. 2025")
+st.write("(tabbybot et al. 2025)")
 url = st.text_input("Paste a news article URL here")
 
 if url:
